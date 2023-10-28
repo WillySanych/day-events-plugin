733cd78aa4a9dffff49e20102102e49dad8c80dc
willysanych <>
2023-10-28 14:06:30 +0300

    Initial commit

---
+++ src/main/java/com/willysanych/dayeventsplugin/DayEventsPluginApplication.java
@@ -0,0 +1,13 @@
+package com.willysanych.dayeventsplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayEventsPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayEventsPluginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willysanych/dayeventsplugin/DayEventsPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willysanych.dayeventsplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayEventsPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
